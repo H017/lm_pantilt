1c4901c46f49667c26e490ce6aee66af08538c83
Gabriel Kenderessy <g@brie.lk>
2014-07-21 14:55:06 -0400

    Started TF  broadcasting

--- scripts/PTU.py
+++ scripts/PTU.py
@@ -2,6 +2,7 @@
 
 from lm_pantilt.srv import Reset, ResetResponse, Pan, PanResponse, Tilt, TiltResponse, PanTilt, PanTiltResponse
 import rospy
+import tf
 import math
 import Adafruit_BBIO.PWM as PWM
 
@@ -17,7 +18,6 @@ class PTU:
         self.pan_max_angle = rospy.get_param('~pan_max_angle', 90)
 
         self.tilt_pin = rospy.get_param('~tilt_pin', 'P9_14')
-        # Need to test
         self.tilt_neutral_duty_cycle = rospy.get_param('~tilt_neutral_duty_cycle', 11.03)
         self.tilt_min_duty_cycle = rospy.get_param('~tilt_min_duty_cycle', 4.6)
         self.tilt_max_duty_cycle = rospy.get_param('~tilt_max_duty_cycle', 10.9)
@@ -25,12 +25,17 @@ class PTU:
         self.tilt_max_angle = rospy.get_param('~tilt_max_angle', 30)
 
         self.frequency = rospy.get_param('~frequency', 50)
+        self.tilt_arm_height = rospy.get_param('~tilt_arm_height', 0.03)
+        self.pan_value = 0
+        self.tilt_value = 0
+
+        self.tf_publisher = tf.TransformBroadcaster()
         
         rospy.Service('~reset', Reset, self.reset)
         rospy.Service('~pan', Pan, self.pan_service)
         rospy.Service('~tilt', Tilt, self.tilt_service)
         rospy.Service('~pan_tilt', PanTilt, self.pan_tilt)
-        
+
         PWM.start(self.tilt_pin, self.tilt_neutral_duty_cycle, self.frequency)
         PWM.start(self.pan_pin, self.pan_neutral_duty_cycle, self.frequency)
         
@@ -53,7 +58,8 @@ class PTU:
             angle = self.pan_min_angle
             
         PWM.set_duty_cycle(self.pan_pin, -angle * ((self.pan_max_duty_cycle - self.pan_min_duty_cycle) / (self.pan_max_angle - self.pan_min_angle)) + self.pan_neutral_duty_cycle)
-                
+        self.pan_angle = angle
+
     def pan_service(self, req):
         self.pan(req.angle)
         
@@ -66,9 +72,8 @@ class PTU:
             angle = self.tilt_min_angle
 
         PWM.set_duty_cycle(self.tilt_pin, angle * ((self.tilt_max_duty_cycle - self.tilt_min_duty_cycle) / (self.tilt_max_angle - self.tilt_min_angle)) + self.tilt_neutral_duty_cycle)
-        
-        return TiltResponse()
-        
+        self.tilt_angle = angle
+
     def tilt_service(self, req):
         self.tilt(req.angle)
         
@@ -80,8 +85,18 @@ class PTU:
         PWM.cleanup()
                             
 if __name__ == '__main__':
+    rate = rospy.Rate(10.0)
     ptu = PTU()
-    rospy.spin()
+
+    while not rospy.is_shutdown():
+        ptu.tf_publisher.sendTransform((0, 0, ptu.tilt_arm_height),
+                                        tf.transformations.quaternion_from_euler(0, ptu.tilt_value, ptu.pan_value),
+                                        rospy.Time.now(),
+                                        "ptu",
+                                        "base_ptu")
+
+        rate.sleep()
+
     ptu.stop()
     
     
