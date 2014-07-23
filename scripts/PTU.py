846e164f6558e3c1c85d2348b5c3d86346f24b4f
Gabriel Kenderessy <g@brie.lk>
2014-07-23 16:59:45 -0400

    Start & Stop services + cleanup and couple of comments

--- scripts/PTU.py
+++ scripts/PTU.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from lm_pantilt.srv import Reset, ResetResponse, Pan, PanResponse, Tilt, TiltResponse, PanTilt, PanTiltResponse
+from lm_pantilt.srv import Start, StartResponse, Stop, StopResponse, Reset, ResetResponse, Pan, PanResponse, Tilt, TiltResponse, PanTilt, PanTiltResponse
 from lm_pantilt.msg import PanTiltState
 
 import rospy
@@ -10,6 +10,9 @@ class PTU:
     def __init__(self):
         rospy.init_node('ptu')
 
+        self.started = False
+
+        # Pan parameters
         self.pan_pin = rospy.get_param('~pan_pin', 'P8_13')
         self.pan_neutral_duty_cycle = rospy.get_param('~pan_neutral_duty_cycle', 7.4)
         self.pan_min_duty_cycle = rospy.get_param('~pan_min_duty_cycle', 2.7)
@@ -17,6 +20,7 @@ class PTU:
         self.pan_min_angle = rospy.get_param('~pan_min_angle', -90)
         self.pan_max_angle = rospy.get_param('~pan_max_angle', 90)
 
+        # Tilt parameters
         self.tilt_pin = rospy.get_param('~tilt_pin', 'P9_14')
         self.tilt_neutral_duty_cycle = rospy.get_param('~tilt_neutral_duty_cycle', 11.03)
         self.tilt_min_duty_cycle = rospy.get_param('~tilt_min_duty_cycle', 6.2)
@@ -24,71 +28,100 @@ class PTU:
         self.tilt_min_angle = rospy.get_param('~tilt_min_angle', -30)
         self.tilt_max_angle = rospy.get_param('~tilt_max_angle', 90)
 
+        # Global PWM parameters
         self.frequency = rospy.get_param('~frequency', 50)
-        self.tilt_arm_height = rospy.get_param('~tilt_arm_height', 0.03)
 
+        # PTU angle publisher parameters
         self.ptu_publisher = rospy.Publisher('/ptu/state', PanTiltState, queue_size=10, latch=True)
         self.pantilt_state = PanTiltState()
         self.pantilt_state.pan = 0
         self.pantilt_state.tilt = 0
-        
+
+        # Services
+        rospy.Service('~start', Start, self.start_service)
+        rospy.Service('~stop', Stop, self.stop_service)
         rospy.Service('~reset', Reset, self.reset)
         rospy.Service('~pan', Pan, self.pan_service)
         rospy.Service('~tilt', Tilt, self.tilt_service)
         rospy.Service('~pan_tilt', PanTilt, self.pan_tilt)
-
-        PWM.start(self.tilt_pin, self.tilt_neutral_duty_cycle, self.frequency)
-        PWM.start(self.pan_pin, self.pan_neutral_duty_cycle, self.frequency)
-
-        self.publish_ptu_state(0, 0)
         
     def reset(self, req):
-        self.pan(0)
-        self.tilt(0)
+        response = ResetResponse()
+        response.success = True
 
-        self.publish_ptu_state(0, 0)
+        if self.started:
+            self.pan(0)
+            self.tilt(0)
+
+            self.publish_ptu_state(0, 0)
+        else:
+            rospy.logerr("PTU not started. Call the 'start' service.")
+            response.success = False
         
-        return ResetResponse()
+        return response
 
     def pan_tilt(self, req):
-        self.pan(req.panAngle)
-        self.tilt(req.tiltAngle)
+        response = PanTiltResponse()
+        response.success = True
 
-        self.publish_ptu_state(req.panAngle, req.tiltAngle)
+        if self.started:
+            self.pan(req.panAngle)
+            self.tilt(req.tiltAngle)
+
+            self.publish_ptu_state(req.panAngle, req.tiltAngle)
+        else:
+            rospy.logerr("PTU not started. Call the 'start' service.")
+            response.success = False
         
-        return PanTiltResponse()
+        return response
 
     def pan(self, angle):
         if(angle > self.pan_max_angle):
             angle = self.pan_max_angle
+            rospy.loginfo("Angle requested is greater than the max allowed angle.")
         elif(angle < self.pan_min_angle):
             angle = self.pan_min_angle
+            rospy.loginfo("Angle requested is less than the min allowed angle.")
             
         PWM.set_duty_cycle(self.pan_pin, -angle * ((self.pan_max_duty_cycle - self.pan_min_duty_cycle) / (self.pan_max_angle - self.pan_min_angle)) + self.pan_neutral_duty_cycle)
         self.pan_angle = angle
 
     def pan_service(self, req):
-        self.pan(req.angle)
+        response = PanResponse()
+        response.success = True
 
-        self.publish_ptu_state(pan=req.angle)
+        if self.started:
+            self.pan(req.angle)
+            self.publish_ptu_state(pan=req.angle)
+        else:
+            rospy.logerr("PTU not started. Call the 'start' service.")
+            response.success = False
         
-        return PanResponse()
+        return response
 
     def tilt(self, angle):
         if(angle > self.tilt_max_angle):
             angle = self.tilt_max_angle
+            rospy.loginfo("Angle requested is greater than the max allowed angle.")
         elif(angle < self.tilt_min_angle):
             angle = self.tilt_min_angle
+            rospy.loginfo("Angle requested is less than the min allowed angle.")
 
         PWM.set_duty_cycle(self.tilt_pin, -angle * ((self.tilt_max_duty_cycle - self.tilt_min_duty_cycle) / (self.tilt_max_angle - self.tilt_min_angle)) + self.tilt_neutral_duty_cycle)
         self.tilt_angle = angle
 
     def tilt_service(self, req):
-        self.tilt(req.angle)
+        response = TiltResponse()
+        response.success = True
 
-        self.publish_ptu_state(tilt=req.angle)
+        if self.started:
+            self.tilt(req.angle)
+            self.publish_ptu_state(tilt=req.angle)
+        else:
+            rospy.logerr("PTU not started. Call the 'start' service.")
+            response.success = False
         
-        return TiltResponse()
+        return response
 
     def publish_ptu_state(self, pan=None, tilt=None):
         if(pan is not None):
@@ -98,11 +131,38 @@ class PTU:
             self.pantilt_state.tilt = tilt
 
         self.ptu_publisher.publish(self.pantilt_state)
-        
+
+    def stop_service(self, req):
+        response = StopResponse()
+        response.success = False
+
+        if self.started:
+            self.stop()
+            response.success = True
+
+        return response
+
     def stop(self):
-        PWM.stop(self.pan_pin) 
+        PWM.stop(self.pan_pin)
         PWM.stop(self.tilt_pin)
         PWM.cleanup()
+        self.started = False
+
+    def start_service(self, req):
+        response = StartResponse()
+        response.success = False
+
+        if not self.started:
+            self.start()
+            response.success = True
+
+        return response
+
+    def start(self):
+        PWM.start(self.tilt_pin, self.tilt_neutral_duty_cycle, self.frequency)
+        PWM.start(self.pan_pin, self.pan_neutral_duty_cycle, self.frequency)
+        self.publish_ptu_state(0, 0)
+        self.started = True
                             
 if __name__ == '__main__':
     ptu = PTU()
