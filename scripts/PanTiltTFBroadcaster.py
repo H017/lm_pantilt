83ec5ecf677016c57a5da533c6a5f78dbde221c0
Ibex-2 <capra@ens.etsmtl.ca>
2014-07-27 13:23:56 -0400

    Modified tf broadcaster

--- scripts/PanTiltTFBroadcaster.py
+++ scripts/PanTiltTFBroadcaster.py
@@ -16,30 +16,27 @@ if __name__ == '__main__':
     pan_rotation = 0
     tilt_rotation = 0
     pan_height = rospy.get_param('~pan_height', 0.062)
-    pan_translation = rospy.get_param('~pan_translation', 0)
     tilt_height = rospy.get_param('~tilt_height', 0.038)
-    attachment_translation = tilt_height + rospy.get_param('~attachment_translation', 0)
-    attachment_frame_name = rospy.get_param('~attachment_frame_name', "attachment_frame")
 
     rospy.Subscriber('/ptu/state',
                      PanTiltState,
                      pantilt_state_callback)
 
-    ptu_frame_broadcaster = tf.TransformBroadcaster()
-    attachment_frame_broadcaster = tf.TransformBroadcaster()
-    r = rospy.Rate(10)
+    broadcaster = tf.TransformBroadcaster()
+    r = rospy.Rate(100)
 
     while not rospy.is_shutdown():
-        ptu_frame_broadcaster.sendTransform((0, 0, pan_translation),
+
+        broadcaster.sendTransform((0, 0, pan_height),
             tf.transformations.quaternion_from_euler(0, tilt_rotation, pan_rotation),
             rospy.Time.now(),
-            "ptu_frame",
-            "base_link")
+            "ptu_pan_tilt",
+            "ptu")
 
-        attachment_frame_broadcaster.sendTransform((0, 0, attachment_translation),
+        broadcaster.sendTransform((0, 0, tilt_height),
             tf.transformations.quaternion_from_euler(0, 0, 0),
             rospy.Time.now(),
-            attachment_frame_name,
-            "ptu_frame")
+            "ptu_attachment",
+            "ptu_pan_tilt")
 
         r.sleep()
