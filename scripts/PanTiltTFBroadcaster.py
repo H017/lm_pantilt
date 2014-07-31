31bbb0f0ae2b71fe49da2470891f8850fe9b7b00
Ibex-2 <capra@ens.etsmtl.ca>
2014-07-31 16:32:00 -0400

    Corrected tf bug

--- scripts/PanTiltTFBroadcaster.py
+++ scripts/PanTiltTFBroadcaster.py
@@ -3,13 +3,15 @@ from lm_pantilt.msg import PanTiltState
 import rospy
 import tf
 import math
+import numpy
 
 def pantilt_state_callback(pantilt_state):
     global pan_rotation, tilt_rotation
 
     tilt_rotation = pantilt_state.tilt * (math.pi / 180)
     pan_rotation = pantilt_state.pan * (math.pi / 180)
-    
+   
+ 
 if __name__ == '__main__':
     rospy.init_node('pan_tilt_tf_broadcaster')
 
@@ -22,21 +24,15 @@ if __name__ == '__main__':
                      PanTiltState,
                      pantilt_state_callback)
 
-    broadcaster = tf.TransformBroadcaster()
-    r = rospy.Rate(50)
+    tfb = tf.TransformBroadcaster()
+    r = rospy.Rate(20)    
 
     while not rospy.is_shutdown():
-
-        broadcaster.sendTransform((0, 0, pan_height),
-            tf.transformations.quaternion_from_euler(0, -tilt_rotation, pan_rotation),
-            rospy.Time.now(),
-            "ptu_pan_tilt",
-            "ptu")
-
-        broadcaster.sendTransform((0, 0, tilt_height),
-            tf.transformations.quaternion_from_euler(0, 0, 0),
-            rospy.Time.now(),
-            "ptu_attachment",
-            "ptu_pan_tilt")
-
-        r.sleep()
+        t = tf.TransformerROS()
+        t1 = t.fromTranslationRotation((0, 0, pan_height), tf.transformations.quaternion_from_euler(0, -tilt_rotation, pan_rotation))
+        t2 = t.fromTranslationRotation((0, 0, tilt_height),tf.transformations.quaternion_from_euler(0, 0, 0))    
+        t3 = numpy.dot(t1, t2)
+        translation = tf.transformations.translation_from_matrix(t3)
+        rotation = tf.transformations.quaternion_from_matrix(t3)
+        tfb.sendTransform(translation, rotation, rospy.Time.now(), "ptu_attachment", "ptu")
+        r.sleep()                                                    
